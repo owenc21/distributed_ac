e2e6c35ce293727f740949694a269e915698f536
Owen Capell <owencapell21@gmail.com>
2024-03-24 02:49:22 -0400

    added functionality to add resources, users, and attributes (roles) to blockchain

--- blockchain/src/blockchain.cpp
+++ blockchain/src/blockchain.cpp
@@ -4,6 +4,9 @@
 #include <cstdint>
 #include <memory>
 #include <ctime>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 #include "blockchain.hpp"
 
@@ -19,23 +22,100 @@ block::node(uint8_t event, uint8_t status, uint64_t id, uint64_t block_id)
 }
 
 /**
+ * Default constructor for payload
+ * Initialzes fields as null except for type
+*/
+block_payload::payload_struct()
+	: payload_type(PAYLOAD_EMPTY), size(0)
+{
+
+}
+
+/**
+ * Construct for payload
+ * Accepts arguments and initializes given fields
+*/
+block_payload::payload_struct(uint8_t payload_type, std::vector<std::string> attributes, uint64_t size, std::string name)
+	: payload_type(payload_type), attributes(attributes), size(size), name(name)
+{
+
+}
+
+/**
  * Default constructor for Blockchain
  * Initializes underlying structure
 */
-Blockchain::Blockchain(){
+Blockchain::Blockchain()
+{
 	head = std::make_shared<block>(BLOCK_EVT_START, BLOCK_STATUS_ACCEPT, 0, 0);
-	next_free = head;	
+	next_free = head;
+	head->payload = std::make_shared<block_payload>();
+	head->payload->payload_type = PAYLOAD_EMPTY;
 	next_id = 1;
+	total_size = sizeof(head) + sizeof(head->payload);
+	non_payload_size = sizeof(head) + sizeof(head->payload);
 }
 
 /**
  * Adds the given resource to the blockchain (on the end)
  * 
  * @param usr_id	uint64_t of the User ID making the insertion
+ * @param attributes	vector of strings that list the attributes which gain access to the resource
+ * @param size		Size (in bytes) of resource being added
+ * @param name		Name of resource beng added
  * @returns 		negaive integer on failure, non-negative integer on success
 */
-int Blockchain::add_resource(uint64_t usr_id){
+int Blockchain::add_resource(uint64_t usr_id, std::vector<std::string> attributes, uint64_t size, std::string name){
 	next_free->next = std::make_shared<block>(BLOCK_EVT_INSERT, BLOCK_STATUS_ACCEPT, usr_id, next_id++);
+	next_free = next_free->next;
+	next_free->payload = std::make_shared<block_payload>(PAYLOAD_SMARTC, attributes, size, name);
 
-	return 1;
+	/* Include block and resource size for total size */
+	uint64_t cur_block_size = sizeof(next_free) + sizeof(next_free->payload);
+	total_size += cur_block_size + size;
+	non_payload_size += cur_block_size;
+
+	return 0;
+}
+
+/**
+ * Adds the given attribute to the blockchain (on the end)
+ * 
+ * @param usr_id	uint64_t of User ID making the insersion
+ * @param users		vector of strings containing names of users included in attribute
+ * @param num_users	Number of users contained within attribute
+ * @param attribute	Name of attribute (must be unique)
+ * @returns			Negative integer on failure, Non-negative integer on success
+*/
+int Blockchain::add_attribute(uint64_t usr_id, std::vector<std::string> users, uint64_t num_users, std::string attribute){
+	/* Ensure name of attribure not already added */
+	if(attributes.contains(attribute)) return -1;
+
+	next_free->next = std::make_shared<block>(BLOCK_EVT_INSERT, BLOCK_STATUS_ACCEPT, usr_id, next_id++);
+	next_free = next_free->next;
+	next_free->payload = std::make_shared<block_payload>(PAYLOAD_ATTRIBUTE, users, num_users, attribute);
+
+	/* Exclude payload from size calculations for purposes of simulation */
+	uint64_t cur_block_size = sizeof(next_free) + sizeof(next_free->payload);
+	total_size += cur_block_size;
+	non_payload_size += cur_block_size;
+
+	return 0;
+}
+
+/**
+ * Adds the given user (name and ID) to Blockchain metadata (represented as fields in class)
+ * 
+ * @param usr_id	uint64_t of User ID of User being added
+ * @param name		Name of user being added (must be unique)
+ * @returns 		Negative integer on failure, Non-negative integer on success
+*/
+int Blockchain::add_user(uint64_t usr_id, std::string name){
+	/* Check if name to be inserted already exists */
+	if(name2id.find(name) == name2id.end()) return -1;
+
+	name2id[name] = usr_id;
+	id2name[usr_id] = name;
+
+	return 0;
 }
--- blockchain/src/blockchain.hpp
+++ blockchain/src/blockchain.hpp
@@ -6,10 +6,16 @@
 /**
  * @brief Node definition for Blockchain
 */
+#ifndef BLOCKCHAIN
+#define BLOCKCHAIN
 
 #include <cstdint>
 #include <memory>
 #include <ctime>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 /* Macros for Block events and statuses */
 #define BLOCK_EVT_START	0
@@ -18,6 +24,12 @@
 
 #define BLOCK_STATUS_ACCEPT 0
 #define BLOCK_STATUS_REJECT 1
+
+/* Macros for Payload Types */
+#define PAYLOAD_EMPTY 0
+#define PAYLOAD_SMARTC 1
+#define PAYLOAD_REQ 2
+#define PAYLOAD_ATTRIBUTE 3
 
 /* Type definitions */
 
@@ -28,6 +40,7 @@ struct node{
 	node() = delete;
 	node(uint8_t event, uint8_t status, uint64_t id, uint64_t block_id);
 	std::shared_ptr<struct node> next;
+	std::shared_ptr<block_payload> payload;
 	std::time_t time_stamp;
 	uint8_t event;
 	uint8_t status;
@@ -36,6 +49,22 @@ struct node{
 };
 
 typedef struct node block;
+
+/**
+ * @brief Definition for payload struct within block
+*/
+
+struct payload_struct{
+	payload_struct();
+	payload_struct(uint8_t payload_type, std::vector<std::string> attributes, uint64_t size, std::string name);
+	uint8_t payload_type;
+	std::vector<std::string> attributes;
+	std::string name;
+	std::string desc;
+	uint64_t size;
+};
+
+typedef struct payload_struct block_payload;
 
 /* Blockchain declaration */
 
@@ -47,11 +76,28 @@ public:
 	/**
 	 * @brief Adds the given resource to the Blockchain
 	*/
-	int add_resource(uint64_t usr_id);
+	int add_resource(uint64_t usr_id, std::vector<std::string> attributes, uint64_t size, std::string name);
+
+	/**
+	 * @brief Adds the given attribute and users associated to the Blockchain
+	*/
+	int add_attribute(uint64_t usr_id, std::vector<std::string> users, uint64_t num_users, std::string attribute);
+
+	/**
+	 * @brief Adds the given user (name and ID) to the Blockchain metadata
+	*/
+	int add_user(uint64_t usr_id, std::string name);
 
 private:
 	std::shared_ptr<block> head;
 	std::shared_ptr<block> next_free;
+	std::unordered_map<uint64_t, std::string> id2name;
+	std::unordered_map<std::string, uint64_t> name2id;
+	std::unordered_set<std::string> attributes;
 	uint64_t next_id;
+	uint64_t total_size;
+	uint64_t non_payload_size;
 
 };
+
+#endif
